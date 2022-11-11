7c9f4a9054cb1508230e0d1e9ed6881427dfbefe
Gabi <gabriela_becher@brown.edu>
2022-11-10 19:12:32 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import bakeryItem from "./components/BakeryItem"
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -10,19 +12,28 @@ bakeryData.forEach((item) => {
 
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
+  const [cart, setCart] = useState([]);
+  const [totPrice, setTotPrice] = useState(0);
   /* add your cart state code here */
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      {bakeryData.map((item, index) => (
+        <BakeryItem name={item.name} 
+                    price={item.price} 
+                    image={item.image} 
+                    cart={cart} 
+                    setCart={setCart}
+                    setTotPrice={setTotPrice}
+                    totPrice={totPrice}/>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div>{cart}</div>
+        <div>Total {totPrice}</div>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,23 @@
-// TODO: create a component that displays a single bakery item
+//import { useState } from "react";
+
+import { useState } from "react";
+
+
+export default function BakeryItem({name, price, image, cart, setCart, totPrice, setTotPrice}) {
+
+    const [index, setIndex] = useState(0);
+
+    const handleClick = () => {
+        setTotPrice(totPrice + price)
+        setCart([...cart, name, " " + index, <br/>])
+        setIndex(index + 1)
+    }
+
+    return(
+        <div> 
+            <div>{name} {price} </div>
+            <img src = {image}/>
+            <button onClick={handleClick}>add to cart</button>
+        </div>
+    )
+}
